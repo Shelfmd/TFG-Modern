751da1f251cdc30012a2d5b9e12c4dbebe8afaa8
NotAShelf <raf@notashelf.dev>
2025-08-15 20:49:38 +0300

    set up devshell for Pakku

---
+++ shell.nix
@@ -0,0 +1,25 @@
+with (import <nixpkgs> {}); let
+  pakkuJar = let
+    version = "v1.2.1";
+  in
+    fetchurl {
+      url = "https://github.com/juraj-hrivnak/Pakku/releases/download/${version}/pakku.jar";
+      sha256 = "sha256-kn87dVGtjRYvlpRYbgq/khzHEIUDChAYK6NqFXVn3gg=";
+    };
+in
+  mkShell {
+    name = "tfg-dev";
+    packages = [
+      # Used for he documentation script.
+      python3
+
+      # Pakku is not available in Nixpkgs. 'tis a shame.
+      (writeShellApplication {
+        name = "pakku";
+        runtimeInputs = [jre];
+        text = ''
+          java -jar ${pakkuJar} "$@"
+        '';
+      })
+    ];
+  }
